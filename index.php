a00ea7dbf778e5b066270a3210386092331d3be1
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-05-21 16:27:40 -0300

    feat: add "Under Construction" page
    This commit adds a simple "Under Construction" page to the site.

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./assets/css/output.css">
+  <title>My Learning Passport</title>
+</head>
+
+<body class="bg-slate-900">
+  <div class="flex flex-col justify-center items-center h-screen text-center">
+    <h2 class="font-mono text-3xl text-white font-bold">My Learning Passport</h2>
+    <img src="assets/svg/barrier.svg" alt="Barrier icon." class="w-52 my-6">
+    <h3 class="font-mono text-xl text-white font-bold">UNDER CONSTRUCTION</h3>
+    <p class="font-mono text-white mt-20">Stay tunned to our
+      <a href="https://github.com/reenatoteixeira/my-learning-passport" target="_blank" class="underline hover:text-gray-500">GitHub</a>
+    </p>
+  </div>
+</body>
+
+</html>
