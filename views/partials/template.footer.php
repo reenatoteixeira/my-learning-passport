158c649cc93cecced47e35225eab037b8671e6e4
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-05-22 20:16:38 -0300

    feat: add footer as template

---
+++ views/partials/template.footer.php
@@ -0,0 +1,12 @@
+</main>
+<footer class="absolute bottom-0 w-full h-20 flex items-center justify-center bg-slate-700">
+  <div class="text-center text-slate-400">
+    <p>My Learning Passport</p>
+    <p>&copy; <?= date("Y") ?>, Renato Teixeira</p>
+  </div>
+</footer>
+
+<script src="./assets/js/script.js"></script>
+</body>
+
+</html>
