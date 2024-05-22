f2e141001981b70bda5acc56979c9fb9e2bb4e88
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-05-22 20:17:43 -0300

    refactor: make header as template

---
+++ views/partials/template.head.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
+  <link rel="stylesheet" href="./assets/css/output.css">
+  <title>My Learning Passport</title>
+</head>
+
+<body class="min-h-screen relative bg-slate-900">
+
+  <?php require_once(__DIR__ . '/template.navbar.php'); ?>
