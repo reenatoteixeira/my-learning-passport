b0a8ae9d82ef3234be2340aa3842dd47de3b230d
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-06-18 16:41:37 -0300

    chore: replace Tailwind CLI with CDN

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./**/*.{php,html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
--- views/partials/head.php
+++ views/partials/head.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
   <link rel="shortcut icon" href="./assets/svg/logo.svg" type="image/x-icon">
-  <link rel="stylesheet" href="/assets/css/style.css">
+  <script src="https://cdn.tailwindcss.com"></script>
   <title>My Learning Passport</title>
 </head>
 
