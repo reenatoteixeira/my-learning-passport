bbfc4acba3b136ee71703e84640e46a5334149c4
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-06-18 16:44:58 -0300

    chore: add about page route

--- config/router.php
+++ config/router.php
@@ -3,6 +3,7 @@ $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 $routes = [
   '/' => './controllers/home.php',
+  '/about' => './controllers/about.php',
 ];
 
 routeToController($uri, $routes);
