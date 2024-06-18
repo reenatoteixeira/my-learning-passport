4d00995d30c93799ca656ef76a64653bf6934e51
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-06-18 16:49:37 -0300

    chore: add teacher page view

---
+++ views/teacher.php
@@ -0,0 +1,14 @@
+<?php
+require_once(__DIR__ . '/partials/head.php');
+require_once(__DIR__ . '/partials/navbar.php');
+?>
+
+<main class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+  <section class="flex flex-col justify-center items-center h-screen text-center">
+    <p class="text-base font-semibold text-sky-500">Hey! Nice to see you here.</p>
+    <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">"Teacher" page is under construction</h1>
+    <p class="mt-6 text-base leading-7 text-gray-600 px-8">You can still access other pages through the nav bar.</p>
+  </section>
+</main>
+
+<?php require_once(__DIR__ . '/partials/footer.php'); ?>
