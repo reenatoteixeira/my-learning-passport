6f3b7d0bbc877a31f702632d42bb2a51db3b3744
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-06-18 16:46:10 -0300

    chore: add contact page route

--- config/router.php
+++ config/router.php
@@ -4,6 +4,7 @@ $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $routes = [
   '/' => './controllers/home.php',
   '/about' => './controllers/about.php',
+  '/contact' => './controllers/contact.php',
 ];
 
 routeToController($uri, $routes);
