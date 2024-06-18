b0f02f8de88d5a8ca5d471236a37daa9190b00ae
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-06-18 16:29:08 -0300

    chore: update footer file

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,5 +1,4 @@
-</main>
-<footer class="absolute bottom-0 w-full h-20 flex items-center justify-center bg-slate-700">
+<footer class="absolute bottom-0 w-full h-20 flex items-center justify-center">
   <div class="text-center text-slate-400">
     <p>My Learning Passport</p>
     <p>&copy; <?= date("Y") ?>, Renato Teixeira</p>
