8db07d53a5557a2153834a1ea7106c9bec9af7f1
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-07-22 19:34:31 -0300

    refactor: add footer partial

---
+++ views/partials/footer.php
@@ -0,0 +1,10 @@
+<footer class="absolute bottom-0 w-full h-20 flex items-center justify-center">
+  <div class="text-center text-slate-400">
+    <p>My Learning Passport</p>
+    <p>&copy; <?= date("Y") ?>, Renato Teixeira</p>
+  </div>
+</footer>
+
+<script src="assets/js/script.js"></script></body>
+
+</html>
