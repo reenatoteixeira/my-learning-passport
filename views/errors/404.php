c6b92cd1f9c63494d4925336f05e7c3ef2ff2ae2
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-07-22 20:33:37 -0300

    chore: add 404 (Page not found) view

---
+++ views/errors/404.php
@@ -0,0 +1,17 @@
+<?php
+require base_path('views/partials/head.php');
+require base_path('views/partials/nav.php');
+?>
+
+<main class="pt-20 text-white">
+  <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+    <h1 class="text-2xl font-bold">Sorry. Page not found.</h1>
+    <p>
+      <a href="/" class="text-blue-500 underline">Go back to home.</a>
+    </p>
+  </div>
+</main>
+
+<?php
+require base_path('views/partials/footer.php');
+?>
