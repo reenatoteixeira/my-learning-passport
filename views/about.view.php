b706f5ced84e4a8a80a39c98541b88104c6fae0e
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-07-22 22:37:49 -0300

    feat: add "About" page controller and view

---
+++ views/about.view.php
@@ -0,0 +1,16 @@
+<?php
+require base_path('views/partials/head.php');
+require base_path('views/partials/nav.php');
+?>
+
+<main class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+  <section class="flex flex-col justify-center items-center h-screen text-center">
+    <p class="text-base font-semibold text-sky-500">Hey! Nice to see you here.</p>
+    <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">"About" page is under construction</h1>
+    <p class="mt-6 text-base leading-7 text-gray-600 px-8">You can still access other pages through the nav bar.</p>
+  </section>
+</main>
+
+<?php
+require base_path('views/partials/footer.php');
+?>
