7dadfb125e0e61cbb75423e831e899a60f1dba6d
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-07-25 13:38:51 -0300

    feat: add teacher's register page view

---
+++ views/teacher/register.view.php
@@ -0,0 +1,84 @@
+<?php
+require base_path('views/partials/head.php');
+?>
+
+<main class="min-h-screen bg-slate-900 px-4 sm:px-6 lg:px-8">
+  <section class="flex flex-col justify-center pb-28 items-center h-screen">
+    <div class="w-full max-w-xl mb-4 sm:mx-auto">
+      <a href="/" class="text-white hover:text-gray-300">
+        🡰 Back to home
+      </a>
+    </div>
+    
+    <div class="w-full max-w-xl bg-white p-4 shadow-lg rounded-xl sm:mx-auto">
+      <form action="" method="post">
+        <div class="mb-4">
+          <label for="first-name" class="block text-sm font-medium leading-6 text-gray-900">
+            First name
+          </label>
+          <input type="text" name="first-name" id="first-name" maxlength="255" placeholder="John" required class="block
+          w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300
+          placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-500 sm:leading-6">
+        </div>
+        
+        <div class="mb-4">
+          <label for="last-name" class="block text-sm font-medium leading-6 text-gray-900">
+            Last name
+          </label>
+          <input type="text" name="last-name" id="last-name" maxlength="255" placeholder="Doe" required class="block
+          w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300
+          placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-500 sm:leading-6">
+        </div>
+        
+        <div class="mb-4">
+          <label for="email" class="block text-sm font-medium leading-6 text-gray-900">
+            Email
+          </label>
+          <input type="email" name="email" id="email" maxlength="255" placeholder="johndoe@example.com" required class="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset
+                 ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-500 sm:leading-6">
+        </div>
+        
+        <div class="mb-4">
+          <label for="password" class="block text-sm font-medium leading-6 text-gray-900">
+            Password
+          </label>
+          <input type="password" name="password" id="password" required class="block
+          w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300
+          placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-500 sm:leading-6">
+        </div>
+        
+        <div class="mb-4">
+          <label for="password-check" class="block text-sm font-medium leading-6 text-gray-900">
+            Verify password
+          </label>
+          <input type="password" name="password-check" id="password-check" required class="block
+          w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300
+          placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-500 sm:leading-6">
+        </div>
+        
+        <hr class="my-4 max-w-96 mx-auto">
+        
+        <div class="mb-4">
+          <button type="submit" class="flex w-full justify-center rounded-md bg-sky-500 px-3 py-1.5 text-sm
+          font-semibold leading-6 text-white shadow-sm hover:bg-sky-400 focus-visible:outline focus-visible:outline-2
+           focus-visible:outline-offset-2 focus-visible:outline-sky-500">
+            Register
+          </button>
+        </div>
+        
+        <div>
+          <p class="text-center text-sm text-gray-500">
+            Already have an account? <a href="/teacher/login" class="font-semibold leading-6 text-sky-500
+            hover:text-sky-400">
+              Login here
+            </a>
+          </p>
+        </div>
+      </form>
+    </div>
+  </section>
+</main>
+
+<?php
+require base_path('views/partials/footer.php');
+?>
