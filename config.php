8c05dd433b38309dcc32c0d972f12e287826f195
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-07-26 18:54:06 -0300

    chore: add `config.php` to access env. varibles

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+require base_path('vendor/autoload.php');
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+return [
+  'database' => [
+    'host' => $_ENV['DB_HOST'],
+    'port' => $_ENV['DB_PORT'],
+    'dbname' => $_ENV['DB_NAME'],
+    'username' => $_ENV['DB_USERNAME'],
+    'password' => $_ENV['DB_PASSWORD'],
+    'charset' => $_ENV['DB_CHARSET'],
+  ],
+];
