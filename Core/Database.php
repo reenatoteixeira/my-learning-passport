d7db6da2591337b9106329b9f51100be9d6f9c3b
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-07-26 19:41:55 -0300

    chore: add `Database` class with a simple query method

---
+++ Core/Database.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Core;
+
+use PDO;
+
+class Database
+{
+  public $conn;
+  public $stmt;
+  
+  public function __construct(array $config)
+  {
+    $dsn = 'mysql:' . http_build_query($config, '', ';');
+    
+    $this->conn = new PDO($dsn, $config['username'], $config['password'],
+      [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
+  }
+  
+  public function query(string $query, array $params = []): static
+  {
+    $this->stmt = $this->conn->prepare($query);
+    $this->stmt->execute($params);
+    
+    return $this;
+  }
+}
