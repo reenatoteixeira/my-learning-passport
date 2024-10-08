26be75458efa76cfbec98c68ef159011c46e2d63
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-10-07 23:19:34 -0300

    feat: add under construction page

---
+++ public/index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="./assets/css/style.css">
+  <title>Document</title>
+</head>
+<body class="bg-slate-900 text-white">
+  <main class="h-screen flex flex-col justify-center items-center text-center">
+    <h1 class="font-mono text-xl text-white font-bold">
+      My Learning Passport </h1>
+    
+    <img src="assets/svg/barrier.svg" alt="Under construction barrier icon" class="size-36 my-6">
+    
+    <h2 class="font-mono text-white font-bold">
+      UNDER CONSTRUCTION </h2>
+    
+    <h3 class="mt-20">
+      Stay tunned to our <a href="https://github.com/reenatoteixeira/my-learning-passport"
+                            class="underline hover:text-gray-400">GitHub</a>
+    </h3>
+  </main>
+</body>
+</html>
