6a7eb33dfa4a900f55d1e33bee3cff0c53a73a1a
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-10-10 22:21:36 -0300

    chore: update Database class

--- Core/Database.php
+++ Core/Database.php
@@ -2,17 +2,19 @@
 
 class Database
 {
-public PDO $connection;
-
-public function __construct($host, $port, $dbname, $user, $password, $charset)
-{
-$dsn = "mysql:host={$host};port={$port};dbname={$dbname};user={$user};password={$password};charset={$charset}";
-
-$this->connection = new PDO($dsn);
-}
-
-public function query(string $query)
-{
-return $this->connection->query($query);
-}
+  public PDO $connection;
+  
+  public function __construct(array $config)
+  {
+    $dsn = 'mysql:' . http_build_query($config, '', ';');
+    
+    $this->connection = new PDO($dsn);
+  }
+  
+  public function query(string $query, array $params = []): false|PDOStatement
+  {
+    $stmt = $this->connection->prepare($query);
+    $stmt->execute($params);
+    return $stmt;
+  }
 }
