e5eeff6ca526107451ccb1b482a26abf92970b0d
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-10-10 22:26:48 -0300

    feat: add teacher's login page

--- views/teacher/login.view.php
+++ views/teacher/login.view.php
@@ -1,13 +1,62 @@
 <?php
 require __DIR__ . '/../partials/head.php';
-require __DIR__ . '/../partials/nav.php';
 ?>
   
-  <main class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-    <section class="flex flex-col justify-center items-center h-screen text-center">
-      <p class="text-base font-semibold text-sky-500">Hey! Nice to see you here.</p>
-      <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Teacher's login page is under construction</h1>
-      <p class="mt-6 text-base leading-7 text-gray-600 px-8">Stay tuned to future updates!</p>
+  <main class="min-h-screen bg-slate-900 px-4 sm:px-6 lg:px-8">
+    <section class="flex flex-col justify-center pb-28 items-center h-screen">
+      <div class="w-full max-w-xl mb-4 sm:mx-auto">
+        <a href="/" class="text-white hover:text-gray-300">
+          🡰 Back to home
+        </a>
+      </div>
+      
+      <div class="w-full max-w-xl bg-white p-4 shadow-lg rounded-xl sm:mx-auto">
+        <form action="" method="post">
+          <div class="mb-4">
+            <label for="email" class="block text-sm font-medium leading-6 text-gray-900">
+              Email
+            </label>
+            <input type="email" name="email" id="email" maxlength="255" placeholder="johndoe@example.com" required class="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset
+                 ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-500 sm:leading-6">
+          </div>
+          
+          <div class="mb-4">
+            <div class="flex items-center justify-between">
+              <label for="password" class="block text-sm font-medium leading-6 text-gray-900">
+                Password
+              </label>
+              
+              <!--
+              <a href="" class="text-sm font-semibold leading-6 text-sky-500 hover:text-sky-400">
+                Forgot password?
+              </a>
+              -->
+            </div>
+            <input type="password" name="password" id="password" placeholder="*********" required class="block
+          w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300
+          placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-500 sm:leading-6">
+          </div>
+          
+          <hr class="my-4 max-w-96 mx-auto">
+          
+          <div class="mb-4">
+            <button type="submit" class="flex w-full justify-center rounded-md bg-sky-500 px-3 py-1.5 text-sm
+          font-semibold leading-6 text-white shadow-sm hover:bg-sky-400 focus-visible:outline focus-visible:outline-2
+           focus-visible:outline-offset-2 focus-visible:outline-sky-500">
+              Sign in
+            </button>
+          </div>
+          
+          <div>
+            <p class="text-center text-sm text-gray-500">
+              Don't have an account? <a href="/teacher/register" class="font-semibold leading-6 text-sky-500
+            hover:text-sky-400">
+                Register here
+              </a>
+            </p>
+          </div>
+        </form>
+      </div>
     </section>
   </main>
 
