9b8a423c552d7a3610436e00a8a15bc5c0894c2a
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-10-14 23:03:24 -0300

    chore: update router to require new routes file

--- Core/router.php
+++ Core/router.php
@@ -1,13 +1,4 @@
 <?php
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-$routes = [
-  '/' => '../Http/controllers/index.php',
-  '/about' => '../Http/controllers/about.php',
-  '/contact' => '../Http/controllers/contact.php',
-  '/student/login' => '../Http/controllers/student/login.php',
-  '/teacher/login' => '../Http/controllers/teacher/login.php',
-  '/teacher/register' => '../Http/controllers/teacher/register.php',
-];
-
+$routes = require('../routes.php');
 routeToController($uri, $routes);
