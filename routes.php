6994a7bc228461be37d5c79e58a248ea75b15d9f
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-10-14 23:03:13 -0300

    chore: extract routes to dedicated file

---
+++ routes.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+  '/' => '../Http/controllers/index.php',
+  '/about' => '../Http/controllers/about.php',
+  '/contact' => '../Http/controllers/contact.php',
+  '/student/login' => '../Http/controllers/student/login.php',
+  '/teacher/login' => '../Http/controllers/teacher/login.php',
+  '/teacher/register' => '../Http/controllers/teacher/register.php',
+];
