64de64ba0982d45191c080b9980ecfbbe12a8edb
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-10-17 21:29:45 -0300

    refactor: update routes file to handle new Router class

--- routes.php
+++ routes.php
@@ -1,10 +1,10 @@
 <?php
 
-return [
-  '/' => 'Http/controllers/index.php',
-  '/about' => 'Http/controllers/about.php',
-  '/contact' => 'Http/controllers/contact.php',
-  '/student/login' => 'Http/controllers/student/login.php',
-  '/teacher/login' => 'Http/controllers/teacher/login.php',
-  '/teacher/register' => 'Http/controllers/teacher/register.php',
-];
+$router->get('/', 'Http/controllers/index.php');
+$router->get('/about', 'Http/controllers/about.php');
+$router->get('/contact', 'Http/controllers/contact.php');
+
+$router->get('/student/login', 'Http/controllers/student/login.php');
+
+$router->get('/teacher/login', 'Http/controllers/teacher/login.php');
+$router->get('/teacher/register', 'Http/controllers/teacher/register.php');
