ea321f18f334b9ff3fb7b7b9d00ed5fe8be3a01f
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-10-17 21:30:00 -0300

    refactor: remove router function

--- Core/functions.php
+++ Core/functions.php
@@ -15,15 +15,6 @@ function isActive(string $uri): bool
   return parse_url($_SERVER['REQUEST_URI'])['path'] === $uri;
 }
 
-function routeToController(string $uri, array $routes): void
-{
-  if (array_key_exists($uri, $routes)) {
-    require basePath($routes[$uri]);
-  } else {
-    abort(Response::NOT_FOUND);
-  }
-}
-
 function abort(int $code): void
 {
   http_response_code($code);
